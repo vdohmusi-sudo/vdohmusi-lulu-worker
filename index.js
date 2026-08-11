0c96ce60eeba89efd385693fc4a1106c1a3eeca3
vdohmusi-sudo <vdohmusi@gmail.com>
2026-08-11 00:24:24 -0700

    Simplify Lulu diagnostic
    Refactored diagnostic response to use variables for key and secret existence checks.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
   }
-};
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
@@ -9,7 +8,8 @@ export default {
       return new Response(
         JSON.stringify({
           status: "online",
-          service: "V. Dohmusi Lulu Fulfillment"
+          service: "V. Dohmusi Lulu Fulfillment",
+          version: "diagnostic-1"
         }),
         {
           headers: {
@@ -20,21 +20,19 @@ export default {
     }
 
     if (url.pathname === "/diagnostic") {
+      const keyExists =
+        typeof env.LULU_CLIENT_KEY === "string" &&
+        env.LULU_CLIENT_KEY.length > 0;
+
+      const secretExists =
+        typeof env.LULU_CLIENT_SECRET === "string" &&
+        env.LULU_CLIENT_SECRET.length > 0;
+
       return new Response(
         JSON.stringify({
           worker: "online",
-
-          LULU_CLIENT_KEY_exists:
-            typeof env.LULU_CLIENT_KEY === "string" &&
-            env.LULU_CLIENT_KEY.length > 0,
-
-          LULU_CLIENT_SECRET_exists:
-            typeof env.LULU_CLIENT_SECRET === "string" &&
-            env.LULU_CLIENT_SECRET.length > 0,
-
-          LULU_BASIC_AUTH_exists:
-            typeof env.LULU_BASIC_AUTH === "string" &&
-            env.LULU_BASIC_AUTH.length > 0
+          LULU_CLIENT_KEY_exists: keyExists,
+          LULU_CLIENT_SECRET_exists: secretExists
         }),
         {
           headers: {
